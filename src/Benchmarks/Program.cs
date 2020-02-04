83f410b944b749d99747379086660ce4c8ecdf41
Adam Sitnik <adam.sitnik@gmail.com>
2020-02-04 14:00:50 +0100

    kill process tree

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -30,6 +31,93 @@ namespace Benchmarks
         public static string Server;
         public static string Protocol;
 
+        private static readonly TimeSpan DefaultKillTimeout = TimeSpan.FromSeconds(30);
+
+        public static void KillTree(Process process, TimeSpan timeout)
+        {
+            var children = new HashSet<int>();
+            GetAllChildIdsUnix(process.Id, children, timeout);
+            foreach (var childId in children)
+            {
+                KillProcessUnix(childId, timeout);
+            }
+            KillProcessUnix(process.Id, timeout);
+        }
+
+        private static int RunProcessAndIgnoreOutput(string fileName, string arguments, TimeSpan timeout)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                RedirectStandardOutput = false,
+                RedirectStandardError = false,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (var process = Process.Start(startInfo))
+            {
+                if (!process.WaitForExit((int)timeout.TotalMilliseconds))
+                    process.Kill();
+
+                return process.ExitCode;
+            }
+        }
+
+        private static void KillProcessUnix(int processId, TimeSpan timeout)
+            => RunProcessAndIgnoreOutput("kill", $"-TERM {processId}", timeout);
+
+        private static void GetAllChildIdsUnix(int parentId, HashSet<int> children, TimeSpan timeout)
+        {
+            var (exitCode, stdout) = RunProcessAndReadOutput("pgrep", $"-P {parentId}", timeout);
+
+            if (exitCode == 0 && !string.IsNullOrEmpty(stdout))
+            {
+                using (var reader = new StringReader(stdout))
+                {
+                    while (true)
+                    {
+                        var text = reader.ReadLine();
+                        if (text == null)
+                            return;
+
+                        if (int.TryParse(text, out int id) && !children.Contains(id))
+                        {
+                            children.Add(id);
+                            // Recursively get the children
+                            GetAllChildIdsUnix(id, children, timeout);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static (int exitCode, string output) RunProcessAndReadOutput(string fileName, string arguments, TimeSpan timeout)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            using (var process = Process.Start(startInfo))
+            {
+                if (process.WaitForExit((int)timeout.TotalMilliseconds))
+                {
+                    return (process.ExitCode, process.StandardOutput.ReadToEnd());
+                }
+                else
+                {
+                    process.Kill();
+                }
+
+                return (process.ExitCode, default);
+            }
+        }
+
         public static void Main(string[] args)
         {
             Args = args;
@@ -57,7 +145,7 @@ namespace Benchmarks
             {
                 try
                 {
-                    dotnet.Kill();
+                    KillTree(dotnet, DefaultKillTimeout);
                 }
                 catch (Exception ex)
                 {
