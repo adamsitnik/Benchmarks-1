0f70baab74d834b3f5a737a77cd0ebeb799fde3e
Adam Sitnik <adam.sitnik@gmail.com>
2020-03-26 10:47:44 +0100

    was it getting GCed?

--- src/BenchmarksApps/Kestrel/PlatformBenchmarks/RawSocketConnection.cs
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/RawSocketConnection.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO.Pipelines;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets;
@@ -11,11 +12,13 @@ namespace PlatformBenchmarks
     {
         private readonly Socket _acceptSocket;
         private readonly SocketTransportOptions _options;
+        private readonly BenchmarkApplication _benchmarkApplication;
 
         public RawSocketConnection(Socket acceptSocket, SocketTransportOptions options)
         {
             _acceptSocket = acceptSocket;
             _options = options;
+            _benchmarkApplication = new BenchmarkApplication(_acceptSocket);
         }
 
         public override string ConnectionId { get; set; }
@@ -23,10 +26,6 @@ namespace PlatformBenchmarks
         public override IDictionary<object, object> Items { get; set; }
         public override IDuplexPipe Transport { get; set; }
 
-        public void Start()
-        {
-            var benchmarkApplication = new BenchmarkApplication(_acceptSocket);
-            benchmarkApplication.ProcessRequestsAsync();
-        }
+        public Task Start() => _benchmarkApplication.ProcessRequestsAsync();
     }
 }
