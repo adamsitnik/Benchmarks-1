c68cfd81aef1b37f90255a92beef72345042ec9a
Adam Sitnik <adam.sitnik@gmail.com>
2020-03-27 18:14:04 +0100

    I forgot to commit sth from the Linux machine

---
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/HttpApplication.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Connections;
+
+namespace PlatformBenchmarks
+{
+    public static class HttpApplicationConnectionBuilderExtensions
+    {
+        public static IConnectionBuilder UseHttpApplication(this IConnectionBuilder builder)
+        {
+            return builder.Use(next => new HttpApplication().ExecuteAsync);
+        }
+    }
+
+    public class HttpApplication
+    {
+        public Task ExecuteAsync(ConnectionContext connection)
+        {
+            var rawSocketConnection = (RawSocketConnection)connection;
+
+            var app = new BenchmarkApplication(rawSocketConnection.Socket);
+
+            return app.StartAsync();
+        }
+    }
+}
