f6e4459f1ef12eec78290b469cf2cdedf00f8a5e
Adam Sitnik <adam.sitnik@gmail.com>
2020-03-30 21:53:29 +0200

    implement IConnectionListenerFactory

---
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/RawSocketTransportFactory.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets;
+using Microsoft.Extensions.Options;
+
+namespace PlatformBenchmarks
+{
+    internal sealed class RawSocketTransportFactory : IConnectionListenerFactory
+    {
+        private readonly SocketTransportOptions _options;
+
+        public RawSocketTransportFactory(IOptions<SocketTransportOptions> options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            _options = options.Value;
+        }
+
+        public ValueTask<IConnectionListener> BindAsync(EndPoint endpoint, CancellationToken cancellationToken = default)
+        {
+            var transport = new RawSocketConnectionListener(endpoint, _options);
+            transport.Bind();
+            return new ValueTask<IConnectionListener>(transport);
+        }
+    }
+}
