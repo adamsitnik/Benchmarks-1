8f8c02c8ab3cd0ac6b9058ef5be2c168f78ac90d
Adam Sitnik <adam.sitnik@gmail.com>
2020-03-31 21:12:16 +0200

    a very desperate try ;)

--- src/BenchmarksApps/Kestrel/PlatformBenchmarks/SocketPipeReader.cs
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/SocketPipeReader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO.Pipelines;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,11 +12,13 @@ namespace PlatformBenchmarks
     {
         private Socket _socket;
         private byte[] _array;
+        private SocketAwaitableEventArgs _awaitableEventArgs;
 
         public SocketPipeReader(Socket socket)
         {
             _socket = socket;
             _array = new byte[16 * 1024];
+            _awaitableEventArgs = new SocketAwaitableEventArgs();
         }
 
         public override void AdvanceTo(SequencePosition consumed) { } // todo: implement
@@ -29,9 +33,83 @@ namespace PlatformBenchmarks
 
         public override async ValueTask<ReadResult> ReadAsync(CancellationToken cancellationToken = default)
         {
-            int bytesRead = await _socket.ReceiveAsync(new Memory<byte>(_array), SocketFlags.None, cancellationToken);
+            var array = _array;
+            var args = _awaitableEventArgs;
+            args.SetBuffer(new Memory<byte>(array));
 
-            return new ReadResult(new System.Buffers.ReadOnlySequence<byte>(_array, 0, bytesRead), isCanceled: false, isCompleted: true);
+            if (_socket.ReceiveAsync(args))
+            {
+                // returns true if the I / O operation is pending
+                await args;
+            }
+
+            return new ReadResult(new System.Buffers.ReadOnlySequence<byte>(array, 0, args.GetResult()), isCanceled: false, isCompleted: true);
+        }
+    }
+
+    internal sealed class SocketAwaitableEventArgs : SocketAsyncEventArgs, ICriticalNotifyCompletion
+    {
+        private static readonly Action _callbackCompleted = () => { };
+
+        private Action _callback;
+
+        public SocketAwaitableEventArgs()
+#if NETCOREAPP5_0
+            : base(unsafeSuppressExecutionContextFlow: true)
+#endif
+        {
+        }
+
+        public SocketAwaitableEventArgs GetAwaiter() => this;
+        public bool IsCompleted => ReferenceEquals(_callback, _callbackCompleted);
+
+        public int GetResult()
+        {
+            //Debug.Assert(ReferenceEquals(_callback, _callbackCompleted));
+
+            _callback = null;
+
+            if (SocketError != SocketError.Success)
+            {
+                ThrowSocketException(SocketError);
+            }
+
+            return BytesTransferred;
+
+            static void ThrowSocketException(SocketError e)
+            {
+                throw new SocketException((int)e);
+            }
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            if (ReferenceEquals(_callback, _callbackCompleted) ||
+                ReferenceEquals(Interlocked.CompareExchange(ref _callback, continuation, null), _callbackCompleted))
+            {
+                Task.Run(continuation);
+            }
+        }
+
+        public void UnsafeOnCompleted(Action continuation)
+        {
+            OnCompleted(continuation);
+        }
+
+        public void Complete()
+        {
+            OnCompleted(this);
+        }
+
+        protected override void OnCompleted(SocketAsyncEventArgs _)
+        {
+            var continuation = Interlocked.Exchange(ref _callback, _callbackCompleted);
+
+            if (continuation != null)
+            {
+                continuation.Invoke();
+                //PipeScheduler.Inline.Schedule(state => ((Action)state)(), continuation);
+            }
         }
     }
 }
