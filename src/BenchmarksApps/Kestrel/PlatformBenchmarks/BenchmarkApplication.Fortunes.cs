ec01dd550d3f1580f27f57213dfcaa4d4a0b06fa
Adam Sitnik <adam.sitnik@gmail.com>
2020-04-16 11:41:49 +0200

    Revert "encode the string in place"
    This reverts commit 0f3254c732823a23181cdc7311cfb4ab94655e3b.

--- src/BenchmarksApps/Kestrel/PlatformBenchmarks/BenchmarkApplication.Fortunes.cs
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/BenchmarkApplication.Fortunes.cs
@@ -48,7 +48,7 @@ namespace PlatformBenchmarks
                 writer.Write(_fortunesRowStart);
                 writer.WriteNumeric((uint)item.Id);
                 writer.Write(_fortunesColumn);
-                writer.EncodeUtf8String(HtmlEncoder, item.Message);
+                writer.WriteUtf8String(HtmlEncoder.Encode(item.Message));
                 writer.Write(_fortunesRowEnd);
             }
             writer.Write(_fortunesTableEnd);
